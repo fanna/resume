26bfd2122dd1b150f6b54d713511b5ed6f8d2de3
Stefan Mijucic <fanna993@gmail.com>
2019-05-20 22:04:56 +0200

    [WIP] Minor style changes

--- js/app.js
+++ js/app.js
@@ -15,50 +15,42 @@ async function app() {
     Elements.titleElement(sidebar, "h4", "contact", "Contact:");
     Elements.titleElement(sidebar, "h5", "phone", resume.contact.phone);
     Elements.titleElement(sidebar, "h5", "email", resume.contact.email);
-    Elements.titleElement(sidebar, "h4", "programming", "Programming Languages:");
-    Elements.listElement(sidebar, "ul", resume.programming.languages, "name");
-    Elements.titleElement(sidebar, "h4", "languages", "Language Skills:");
-    Elements.listElement(sidebar, "ul", resume.languages, "language");
-    Elements.titleElement(sidebar, "h4", "interests", "Interests:");
-    Elements.listElement(sidebar, "ul", resume.interests, "name");
+    Elements.listElement(sidebar, "ul", resume.programming.languages, "name", "Programming Languages:");
+    Elements.listElement(sidebar, "ul", resume.languages, "language", "Language Skills:");
+    Elements.listElement(sidebar, "ul", resume.interests, "name", "Interests");
 
-    Elements.titleElement(main, "h3", "work", "Work Experience:");
     const workKeys = [
-        {"type": "h3", "id": "position"},
-        {"type": "h4", "id": "period"},
-        {"type": "h5", "id": "description"}
-    ];
-    Elements.contentElement(main, resume.work, workKeys, "work");
-
-    Elements.titleElement(main, "h3", "workshops", "Workshops and Seminars:");
-    const workshopKeys = [
-        {"type": "h3", "id": "date"},
-        {"type": "h4", "id": "location"},
-        {"type": "h5", "id": "description"}
-    ];
-    Elements.contentElement(main, resume.workshops, workshopKeys, "workshops");
-
-    Elements.titleElement(main, "h3", "volunteering", "Volunteering:");
-    const volunteeringKeys = [
-        {"type": "h3", "id": "date"},
-        {"type": "h4", "id": "location"},
-        {"type": "h4", "id": "position"},
-        {"type": "h5", "id": "description"}
-    ];
-    Elements.contentElement(main, resume.volunteering, volunteeringKeys, "volunteering");
-
-    Elements.titleElement(main, "h3", "education", "Education:");
-    const educationKeys = [
-        {"type": "h4", "id": "period"},
-        {"type": "h4", "id": "location"},
-        {"type": "h5", "id": "title"},
-        {"type": "h5", "id": "thesis"},
+        {"type": "p", "id": "position"},
+        {"type": "p", "id": "period"},
         {"type": "p", "id": "description"}
     ];
-    Elements.contentElement(main, resume.education, educationKeys, "education");
+    Elements.contentElement(main, resume.work, workKeys, "main", "Work Experience:");
 
-    Elements.titleElement(sidebar, "h4", "social", "Social:");
-    Elements.listElement(sidebar, "ul", resume.social, "link");
+    const workshopKeys = [
+        {"type": "p", "id": "date"},
+        {"type": "p", "id": "location"},
+        {"type": "p", "id": "description"}
+    ];
+    Elements.contentElement(main, resume.workshops, workshopKeys, "main", "Workshops and Seminars:");
+
+    const volunteeringKeys = [
+        {"type": "p", "id": "date"},
+        {"type": "p", "id": "location"},
+        {"type": "p", "id": "position"},
+        {"type": "p", "id": "description"}
+    ];
+    Elements.contentElement(main, resume.volunteering, volunteeringKeys, "main", "Volunteering:");
+
+    const educationKeys = [
+        {"type": "p", "id": "period"},
+        {"type": "p", "id": "location"},
+        {"type": "p", "id": "title"},
+        {"type": "p", "id": "thesis"},
+        {"type": "p", "id": "description"}
+    ];
+    Elements.contentElement(main, resume.education, educationKeys, "main", "Education:");
+
+    Elements.listElement(sidebar, "ul", resume.social, "link", "Social:");
 }
 
 app();
--- js/elements.js
+++ js/elements.js
@@ -7,8 +7,12 @@ export class Elements {
         parent.appendChild(element);
     }
 
-    static async listElement(parent, type, list, key) {
+    static async listElement(parent, type, list, key, title) {
         const element = document.createElement(type);
+        const titleElement = document.createElement("h3");
+        titleElement.append(title);
+        element.appendChild(titleElement);
+
         list.forEach(item => {
             const listItem = document.createElement("li");
             listItem.append(item[key]);
@@ -18,8 +22,12 @@ export class Elements {
         parent.appendChild(element);
     }
 
-    static async contentElement(parent, contents, keys, className) {
+    static async contentElement(parent, contents, keys, className, title) {
         const element = document.createElement("div");
+        const titleElement = document.createElement("h3");
+        titleElement.append(title);
+        element.appendChild(titleElement);
+
         contents.forEach(item => {
             keys.forEach(key => {
                 const innerElement = document.createElement(key.type);
